9aab5bec098a6f46b35d683a7a06cc22b758a6e2
Maxime Lizere <maxime.lizere@oclock.school>
2024-09-02 11:47:57 +0200

    basic setup for api server done

--- back/index.js
+++ back/index.js
@@ -29,7 +29,13 @@ app.use(
 )
 
 
+import { router } from './app/routers/router.js';
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(router);
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+})
