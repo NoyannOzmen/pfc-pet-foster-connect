070015036f2b1dc9c48a04bc37c9965afb429aed
Maxime Lizere <maxime.lizere@oclock.school>
2024-09-02 11:33:29 +0200

    Added basic API setup

---
+++ back/index.js
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import session from 'express-session';
+
+const app = express();
+
+//! Session options, peut être à redéfinir plus tard !
+const sessionOptions = {
+    secret:process.env.SESSION_SECRET,
+    resave:true,
+    saveUninitialized:true,
+    cookie:{
+        maxAge:1000*60*60,
+    //! A CHANGER SI ON A LE CERTIFICAT HTTPS !!!
+        secure:false,
+        sameSite:'strict',
+    }
+}
+
+app.use(session(sessionOptions));
+
+app.use(
+    cors({
+        origin:[
+
+        ]
+    })
+)
+
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(router);
