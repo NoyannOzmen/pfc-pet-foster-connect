c937a4df854dc20b79f545dc7a4d647f74946e53
Maxime Lizere <maxime.lizere@oclock.school>
2024-09-02 16:22:43 +0200

    added error wrapper around animal routes

--- back/app/routers/animalRouter.js
+++ back/app/routers/animalRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { animalController } from "../controllers/animalController";
+import { catchErrors } from "../middlewares/catchErrors";
 
 const animalRouter = Router();
 
-animalRouter.get('/animals', animalController.getAll);
-animalRouter.get('/animals/:id(\\d+)', animalController.getOne)
+animalRouter.get('/animals', catchErrors(animalController.getAll));
+animalRouter.get('/animals/:id(\\d+)', catchErrors(animalController.getOne))
 
