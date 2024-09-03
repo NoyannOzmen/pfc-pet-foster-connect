153a6c5843cbb2050df529732bb7ec9fd47a69af
Samuel Juminer <samuel.juminer@oclock.school>
2024-09-03 16:08:46 +0200

    staticPageRouter fait !

---
+++ src/routers/staticPageRouter.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const staticPageRouter = Router();
+
+staticPageRouter.get('/a-propos', res.render('aPropos'));
+staticPageRouter.get('/contact', res.render('contact'));
+staticPageRouter.get('/faq', res.render('faq'));
+staticPageRouter.get('/infos-legales', res.render('infosLegales'));
+staticPageRouter.get('/rgpd', res.render('rgpd'));
+
+
