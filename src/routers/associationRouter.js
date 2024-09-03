052826f7675bc34729f60aef88b0c1916ae0a542
Samuel Juminer <samuel.juminer@oclock.school>
2024-09-03 15:41:23 +0200

    associationController et  associationRouter voir l'ajout de l'animal à l'asso

--- src/routers/associationRouter.js
+++ src/routers/associationRouter.js
@@ -1,20 +1,26 @@
 import { Router } from "express";
-import { assoController } from "../controllers/associationController.js";
+import { associationController } from "../controllers/associationController.js";
 import { catchErrors } from "../middlewares/catchErrors.js";
 
-const assoRouter = Router();
+const associationRouter = Router();
 
 //Affichage de la liste complète des associations
 associationRouter.get('/api/associations', catchErrors(associationController.getAll));
 
 //Affichage des détails d'une association
-associationRouter.get('/api/associations/{id}', catchErrors(associationController.getOne));
+associationRouter.get('/api/associations/:id(\\d+)', catchErrors(associationController.getOne));
 
 //Soumission du formulaire d'inscription d'association
-associationRouter.post('/assos', catchErrors(associationController.create));
+associationRouter.post('api/associations', catchErrors(associationController.store));
 
 //Mise à jour des informations d'une association
-associationRouter.patch('/api/associations/{id}', catchErrors(associationController.update));
+associationRouter.patch('/api/associations/:id(\\d+)', catchErrors(associationController.update));
 
 //Suppression d'un compte association
-associationRouter.delete('/api/associations/{id}', catchErrors(associationController.destroy));
+associationRouter.delete('/api/associations/:id(\\d+)', catchErrors(associationController.destroy));
+
+//Affichage des animaux proposés par une association
+associationRouter.get('/api/associations/:id(\\d+)/animals', catchErrors(associationController.getAllAnimals));
+
+//Ajout un animal à l'association
+associationRouter.post('/api/associations/:id(\\d+)/animals', catchErrors(associationController.addAnimal));
