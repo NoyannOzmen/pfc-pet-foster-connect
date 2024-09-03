323f08f5475f36040b20cfaf8d5c61d4eb37e0e1
Maxime Lizere <maxime.lizere@oclock.school>
2024-09-03 16:50:56 +0200

    Started working on sessionController, added session parameters to JS entry point

---
+++ src/controllers/sessionController.js
@@ -0,0 +1,17 @@
+import bcrypt from 'bcrypt';
+
+export const sessionController = {
+
+   async login(req,res) {
+
+    // On récupère l'email et le mot de passe
+    const credentials = req.body;
+//TODO VERIFICATION DES CREDENTIALS AVEC JOI
+
+
+
+
+
+
+   }
+}
