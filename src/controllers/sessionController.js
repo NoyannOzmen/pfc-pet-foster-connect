29fc68bc8a1e36272cf55735dfc1138b80a2be86
Maxime Lizere <maxime.lizere@oclock.school>
2024-09-04 09:14:18 +0200

    sessionController not functionnal

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -1,17 +1,17 @@
 import bcrypt from 'bcrypt';
 
 export const sessionController = {
-
-   async login(req,res) {
-
-    // On récupère l'email et le mot de passe
-    const credentials = req.body;
-//TODO VERIFICATION DES CREDENTIALS AVEC JOI
-
-
-
-
-
-
-   }
+    
+    async login(req,res) {
+        
+        // On récupère l'email et le mot de passe
+        const credentials = req.body;
+        //TODO VERIFICATION DES CREDENTIALS AVEC JOI
+        
+        const user = await 
+        
+        
+        
+        
+    }
 }
