00c8438d321cfdb53289d317443e5825bd9e3c30
Maxime Lizere <maxime.lizere@oclock.school>
2024-09-05 11:12:23 +0200

    added docker setup

---
+++ scripts/installation.sh
@@ -0,0 +1,15 @@
+# Installation de Docker et Docker compose
+
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+sudo reboot
+
+# creation du volume persistant sur lequel les données de la BDD seront stockées
+docker volume create pg-pfc
