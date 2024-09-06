5f8b5f5cec7966ea1b57abec6e6170ba52b406fa
NoyannOzmen <noyann.ozmen@oclock.school>
2024-09-06 15:48:37 +0200

    Inscription Asso OK / Login bug

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import emailValidator from 'email-validator';
 
 import { Famille, Utilisateur, Association, Espece } from '../models/Models.js';
 
@@ -8,52 +9,37 @@ export const sessionController = {
         res.render("connexion");
     },
 
-    async displayFosterSignIn(req,res) {
-        res.render("inscriptionFamille")
-    },
-
-    async displayShelterSignIn(req,res) {
-        const especes = await Espece.findAll();
-        res.render("inscriptionAssociation", { especes })
-    },
-    
-    async login(req,res) {
+    async logIn(req,res) {
         
-        // On récupère l'email et le mot de passe
+/*         // On récupère l'email et le mot de passe
         const credentials = req.body;
-        //TODO VERIFICATION DES CREDENTIALS AVEC JOI
+        //TODO VERIFICATION DES CREDENTIALS AVEC JOI */
 
-        const {email, mot_de_passe} = credentials
+        const {
+            email, 
+            mot_de_passe
+        } = req.body
         
         const user = await Utilisateur.findOne({
-
-
         //! ATTENTION : Peut être que les associations ont un problème au niveau lien association/user famille/user
         //! A TESTER
 
             where : {
-                email:email
+                email: email
             },
-            include : [
-                {
-                    model:Association,
-                },
-                {
-                    model:Famille
-                }
-            ]
+            include : ['refuge','accueillant']
         })
+
+/*         console.log('User is', user); */
         
         if (!user) {
             return res.render('connexion', {error : "utilisateur ou mot de passe incorrect"})
-
         }
         
         //* Bcrypt compare le hash du mot de passe récupéré depuis la requète avec celui en BDD
-        const hasMatchingPassword = bcrypt.compare(mot_de_passe, user.mot_de_passe);
+        const hasMatchingPassword = await bcrypt.compare(mot_de_passe, user.mot_de_passe);
 
         if(!hasMatchingPassword) {
-
             return res.render('connexion', {error : "utilisateur ou mot de passe incorrect"})
 
         } else {
@@ -66,26 +52,114 @@ export const sessionController = {
         //*     - nom : pour afficher sur toutes les vues le nom du user
         //*     - id : Pour faciliter les futurs appels BDD pour afficher les infos des profils etc...
 
-            if (user.association.id) {
+        let refugeId = user.refuge.utilisateur_id;
+        let familleId = user.accueillant.utilisateur_id;
+
+        console.log('Refuge is', refugeId, 'Famille is', familleId);
+
+            if (refugeId != null) {
                 req.session.loggedIn=true;
                 req.session.role='association';
-                req.session.nom=user.association.nom;
-                req.session.id=user.id;
+                req.session.nom=user.refuge.nom;
+                req.session.id=refugeId;
             }
-            if (user.famille.id) {
+            if (familleId != null ) {
                 req.session.loggedIn=true;
                 req.session.role='famille';
-                req.session.nom=user.famille.nom;
-                req.session.id=user.id;
+                req.session.nom=user.accueillant.nom;
+                req.session.id=familleId;
             }
-
+            console.log(req.session)
         }
         return res.redirect('/')
     },
 
-    async logout(req,res) {
+    async logOut(req,res) {
 
         req.session.destroy();
         res.redirect('/')
-    }
+    },
+
+    async displayFosterSignIn(req,res) {
+        res.render("inscriptionFamille")
+    },
+
+    async FosterSignIn(req,res) {
+        const newFoster = await Famille.create({
+
+        })
+    },
+
+    async displayShelterSignIn(req,res) {
+        const especes = await Espece.findAll();
+        res.render("inscriptionAssociation", { especes })
+    },
+    
+    async ShelterSignIn(req,res) {
+
+        const { 
+            nom, 
+            responsable, 
+            rue, 
+            commune, 
+            code_postal, 
+            pays, 
+            siret, 
+            telephone, 
+            email, 
+            mot_de_passe, 
+            confirmation 
+        } = req.body;
+
+        const found = await Utilisateur.findOne( { where: {email: email} });
+
+        console.log(found);
+
+        if(found === null) {
+            if (!emailValidator.validate(email)) {
+                return res.render('/association/inscription', {
+                    error: "Cet email n'est pas valide.",
+                });
+            }
+            // verifier si password correspond à password confirm
+            if (mot_de_passe !== confirmation) {
+                return res.render('/association/inscription', {
+                    errorMessage:
+                    'La confirmation du mot de passe ne correspond pas au mot de passe renseigné.',
+                });
+            }
+
+            const encryptedPassword = await bcrypt.hash(mot_de_passe, 8);
+            console.log('HASH', encryptedPassword);
+
+            const newUser = await Utilisateur.create({
+                email: email,
+                mot_de_passe : encryptedPassword,
+            })
+            console.log(newUser);
+            await newUser.save();
+            
+            const newShelter = await Association.create({
+                nom : nom,
+                responsable : responsable,
+                rue : rue,
+                commune : commune,
+                code_postal : code_postal,
+                pays : pays,
+                siret : siret,
+                telephone : telephone,
+                utilisateur_id: newUser.id,
+            });
+            console.log(newShelter);
+            /* req.flash('success', `Merci pour votre inscription !`); */
+            console.log(`C'est good`)
+            await newShelter.save();
+        } else {
+            /* req.flash('success', 'Cet utilisateur existe déjà !'); */
+            console.log('Non')
+        }
+        console.log(found)
+        console.log("Déjà inscrit")
+        res.redirect('/')
+    },
 }
--- src/routers/sessionRouter.js
+++ src/routers/sessionRouter.js
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { sessionController } from "../controllers/sessionController.js";
+import { catchErrors } from "../middlewares/catchErrors.js";
 
 const sessionRouter = Router();
 
-sessionRouter.get('/connexion', sessionController.displayLogin);
-sessionRouter.get('/famille/inscription', sessionController.displayFosterSignIn);
-sessionRouter.get('/association/inscription', sessionController.displayShelterSignIn)
+/* routes connexion */
+sessionRouter.get('/connexion', catchErrors(sessionController.displayLogin));
+sessionRouter.post('/connexion', catchErrors(sessionController.logIn));
+
+/* route déconnexion */
+sessionRouter.get('deconnexion', catchErrors(sessionController.logOut));
+
+/* routes inscription famille d'accueil */
+sessionRouter.get('/famille/inscription', catchErrors(sessionController.displayFosterSignIn));
+sessionRouter.post('/famille/inscription', catchErrors(sessionController.FosterSignIn));
+
+/* routes inscription association */
+sessionRouter.get('/association/inscription', catchErrors(sessionController.displayShelterSignIn));
+sessionRouter.post('/association/inscription', catchErrors(sessionController.ShelterSignIn));
 
 export { sessionRouter };
