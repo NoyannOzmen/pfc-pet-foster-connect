5a86558ab7a881fcfb75ca1b6a63342feceb2e86
Maxime Lizere <maxime.lizere@oclock.school>
2024-09-11 12:01:38 +0200

    Added a modal for adding a tag to bdd

---
+++ src/assets/utils/dashboardAssoCreateAnimal.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    addTag();
+});
+
+
+function addTag() {
+    
+    const addTagBtn = document.getElementById('create-tag');
+
+    addTagBtn.addEventListener('click', ()=>{
+        event.preventDefault();
+
+    })
+}
